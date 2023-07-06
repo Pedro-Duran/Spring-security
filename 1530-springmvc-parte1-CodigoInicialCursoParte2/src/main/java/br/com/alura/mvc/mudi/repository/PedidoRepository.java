bf22de40e1fa3d99ab4dd5094dd49039f5800e65
Flavia-Duran <duranpepeu@gmail.com>
2023-07-06 18:06:39 -0300

    adicionando recurso de paginação e ordenando pedidos cronologicamente

--- 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,9 +15,12 @@ import br.com.alura.mvc.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-	List<Pedido> findByStatus(StatusPedido aguardando);
+	List<Pedido> findByStatus(StatusPedido aguardando, Pageable sort);
 	
 	@Query("Select p from Pedido p join p.user u where u.username = :username")
 	List<Pedido> findAllByUsuario(@Param("username")String username);
+
+	@Query("Select p from Pedido p join p.user u where u.username = :username and p.status = :status")
+	List<Pedido> findByStatusAndUsuario(@Param("status") StatusPedido status,@Param("username") String username);
 	
 }
